b9269daabd46082c22b6f30e67e55d72dae34b7c
Hugues Malphettes <hmalphettes@gmail.com>
2015-11-14 09:44:14 +0800

    Properly render the birthdates and voting timestamps in the collections list

--- ui/collection.js
+++ ui/collection.js
@@ -9,19 +9,18 @@ const columns = [/*'orpcexcel.newmemberid', */ // columns from the orpcexcel tab
   'famname', 'firstname', 'middlename',
   'preferredname', 'birthdate', 'nric', 'membertype', 'orpcexcel.mbrstatus',
   'proxyid', 'desk', 'timestamp', // columns from the voting table
-  'vote_status' ]. // extra computed column
-  map(function(c) {
-    return { title: c };
-  });
+  'vote_status' ]; // extra computed column
+const birthdateIdx = columns.indexOf('birthdate');
+const timestampIdx = columns.indexOf('timestamp');
 
-datatables('#members').DataTable( {
-  processing: true,
-  ajax: '/collection',
-  columns: columns
-});
+// datatables('#members').DataTable( {
+//   processing: true,
+//   ajax: '/collection',
+//   columns: columns
+// });
 
 
-/*
+
 datatables.ajax({
   url: '/collection',
   type: 'get',
@@ -34,13 +33,45 @@ datatables.ajax({
       console.error('Unexpected state', rows);
       return;
     }
+    for (var row of rows) {
+      processRow(row);
+    }
     datatables('#members').DataTable( {
       // processing: true,
       data: rows,
-      columns: columns
+      columns: columns.map(function(c) {
+        return { title: c };
+      })
     });
   },
   error: function() {
     console.log('check error', arguments);
   }
-});*/
+});
+
+/**
+ * timestamp -> rendered as time.
+ * birthdate -> rendered as date
+ */
+function processRow(row) {
+  var b = row[birthdateIdx];
+  if (b) {
+    var bd = new Date(Date.parse(b));
+    if (!isNaN(bd.valueOf())) {
+      row[birthdateIdx] = bd.getFullYear() + '-' + pad(bd.getMonth()) + '-' + pad(bd.getDate());
+    }
+  }
+
+  var t = row[timestampIdx];
+  if (t) {
+    var td = new Date(Date.parse(t));
+    if (!isNaN(td.valueOf())) {
+      row[timestampIdx] = pad(td.getHours()) + ':' + pad(td.getMinutes()) + ':' + pad(td.getSeconds());
+    }
+  }
+
+}
+
+function pad(numb) {
+    return (numb < 10 ? '0' : '') + numb;
+}
